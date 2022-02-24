ce3cfda1fa560520f787f89383b3942b624c480f
eelsinm <eelsinm@gmail.com>
2022-02-25 08:58:29 +0900

    test

--- nomadVanillaJs/js/clock.js
+++ nomadVanillaJs/js/clock.js
@@ -2,9 +2,12 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    const hour = date.getHours();
-    console.log(typeof(hour));
-    clock.innerText = `${hour}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const secondes = String(date.getSeconds()).padStart(2, "0");
+    //console.log(typeof(hour));
+    clock.innerText = `${hours} : ${minutes} : ${secondes}`;
 }
 
 getClock();
+setInterval(getClock, 1000);

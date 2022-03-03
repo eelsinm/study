dba29f3b51f4ba270ca18c7c29bcf6507196f43d
eelsinm <eelsinm@gmail.com>
2022-03-04 08:42:34 +0900

    test

---
+++ nomadPaintJS/app.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel = "stylesheet" href = "style.css" />
+        <title>Paint JS</title>
+    </head>
+    <body>
+
+        <script src = "app.js"></script>
+    </body>
+</html>

fc6cce267e60b8b76ebf3b25d24bed7ab5a88850
eelsinm@gmail.com <eelsinm@gmail.com>
2022-03-09 19:06:14 +0900

    test

--- nomadVanillaJs/js/todo.js
+++ nomadVanillaJs/js/todo.js
@@ -1,6 +1,34 @@
 const todoForm = document.querySelector("#todo-form");
-const todoInput = document.querySelector("#todo-form input")
+const todoInput = document.querySelector("#todo-form input");
+const todoList = document.querySelector("#todo-list");
 let todos = [];
+const TODOS_KEY = "todosK";
+
+function saveTodos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+    saveTodos();
+}
+
+function paintTodo(newTodoObj){
+    const li = document.createElement("li");
+    li.id = newTodoObj.id;
+    const span = document.createElement("span");
+    span.innerText = newTodoObj.text; 
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteTodo);
+
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+    
+}
 
 function onInputTodo(event){
     event.preventDefault();
@@ -11,12 +39,21 @@ function onInputTodo(event){
     todoInput.value = "";
     const newTodoObj = {
         text: newTodo,
-        ID: Date.now()
+        id: Date.now()
     };
     
-    
+    paintTodo(newTodoObj);
     todos.push(newTodoObj);
+    saveTodos();
     console.log(todos);
 }
 
 todoForm.addEventListener("submit", onInputTodo);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null){
+    const parsedTodos = JSON.parse(savedTodos);
+    todos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}

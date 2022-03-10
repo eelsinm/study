e0538f51f8cb5dc1682e58ad6a8d44a73e581ca4
eelsinm <eelsinm@gmail.com>
2022-03-11 08:50:11 +0900

    test

--- nomadPaintJS/app.js
+++ nomadPaintJS/app.js
@@ -1,5 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+console.log(colors);
+let colorArray = Array.from(colors);
+console.log(colorArray);
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -7,13 +12,20 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-//ctx.fillStyle = "white";
-//ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-//ctx.strokeStyle = INITIAL_COLOR;
-//ctx.fillStyle = INITIAL_COLOR;
-//ctx.lineWidth = 2.5;
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    console.log(ctx.strokeStyle)
+  }
 
 function onMouseMove(event) {
     //console.log(event.offsetX, event.offsetY);
@@ -45,3 +57,10 @@ if(canvas) {
     canvas.addEventListener("mouseup", stopPainting);
 
 }
+
+
+
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick)
+    
+  );
